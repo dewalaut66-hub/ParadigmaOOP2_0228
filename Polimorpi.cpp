3ec779a491a7073508542cccb0dd1d68c8997f68
gusti rifqi <dewalaut66@icloud.com>
2026-05-19 10:42:34 +0700

    commit 04

--- Polimorpi.cpp
+++ Polimorpi.cpp
@@ -10,3 +10,13 @@ public:
         cout << "pesan dari seseorang" << endl;
     }
 };
+
+class joko : public seseorang
+{
+public:
+    void pesan()
+    {
+        cout << "pesan dari joko"<< endl;
+    }
+};
+
