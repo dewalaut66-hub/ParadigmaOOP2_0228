ab03cb9801874402dd46587510173b346d3158f1
gusti rifqi <dewalaut66@icloud.com>
2026-05-19 11:46:24 +0700

    commit 20

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -25,4 +25,11 @@ int main()
     lampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
     lampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
     lampuRumah.setSaklarNo(3, "Lampu Dapur");
+
+    cout << "Saklar No 0: " << lampuRumah.getSaklarNo(0) << endl;
+    cout << "Saklar No 1: " << lampuRumah.getSaklarNo(1) << endl;
+    cout << "Saklar No 2: " << lampuRumah.getSaklarNo(2) << endl;
+    cout << "Saklar No 3: " << lampuRumah.getSaklarNo(3) << endl;
+
+    return 0;
 }
